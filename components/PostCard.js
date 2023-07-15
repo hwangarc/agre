ee84dbe7676f2204937aed3ab8c934b81743e098
hwangarc <hoangarc99@gmail.com>
2023-07-15 11:37:22 +0700

    Cập nhật các tệp tin đã chỉnh sửa

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,11 +1,19 @@
 import Link from "next/link"
 
-export default function PostCard ({ post }){
-    return (
-        <Link href={post.uri} className={"card"}>
-            <a className="card">
-                <h3>{post.title} &rarr;</h3>
-            </a>
-        </Link>
-    )
+export default function PostCard({ post }) {
+  const truncatedContent = post.content.slice(0, 100) + "...";
+
+  return (
+    <Link href={post.uri}>
+      <a className="card">
+        {post.featuredImage && post.featuredImage.node.sourceUrl && (
+          <div className="image-container">
+            <img src={post.featuredImage.node.sourceUrl} alt="Feature Image" />
+          </div>
+        )}
+        <h3>{post.title} &rarr;</h3>
+        <p>{truncatedContent}</p>
+      </a>
+    </Link>
+  )
 }
--- pages/[uri].js
+++ pages/[uri].js
@@ -1,71 +1,69 @@
-import Head from 'next/head'
-import Footer from '../components/Footer'
-import { getPostByUri } from '../lib/test-data';
+import Head from 'next/head';
+import Footer from '../components/Footer';
 import { client } from '../lib/apollo';
 import { gql } from '@apollo/client';
 
 export default function SlugPage({ post }) {
-
   return (
     <div>
       <Head>
         <title>Headless WP Next Starter</title>
-        <link rel="icon" href="favicon.ico"></link>
+        <link rel="icon" href="favicon.ico" />
       </Head>
 
       <main>
-          <div className="siteHeader">
-            <h1 className="title">
-                {post.title}
-            </h1>
-            <p>✍️  &nbsp;&nbsp;{`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️ &nbsp;&nbsp;{ new Date(post.date).toLocaleDateString() }</p>
-          </div>
-            <article dangerouslySetInnerHTML={{__html: post.content}}>   
-            </article>
+        <div className="siteHeader">
+          <h1 className="title">{post.title}</h1>
+          <p>
+            ✍️ &nbsp;&nbsp;
+            {`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️ &nbsp;&nbsp;
+            {new Date(post.date).toLocaleDateString()}
+          </p>
+        </div>
+        <article dangerouslySetInnerHTML={{ __html: post.content }}></article>
       </main>
 
       <Footer></Footer>
-
     </div>
-  )
+  );
 }
 
-
-export async function getStaticProps({ params }){
+export async function getStaticProps({ params }) {
   const GET_POST_BY_URI = gql`
-      query NewQuery($id: ID!) {
-        post(id: $id, idType: URI) {
-          title
-          content
-          date
-          uri
-          featuredImageId
-          author {
-            node {
-              firstName
-            }
+    query GetPostByUri($uri: ID!) {
+      post(id: $uri, idType: URI) {
+        title
+        content
+        date
+        author {
+          node {
+            firstName
+            lastName
           }
         }
       }
-  `
+    }
+  `;
+
   const response = await client.query({
     query: GET_POST_BY_URI,
     variables: {
-      id: params.uri
-    }
-  })
-  const post = response?.data?.post
+      uri: params.uri,
+    },
+  });
+
+  const post = response?.data?.post;
+
   return {
     props: {
-      post
-    }
-  }
+      post,
+    },
+  };
 }
 
-export async function getStaticPaths(){
-    const paths = []
-    return {
-        paths,
-        fallback: 'blocking'
-    }
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
 }
